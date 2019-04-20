daf99c711047d8f0955714f6f428aac1ca5b9197
jiangxingqi <shixi_xingqi@sina.com.cn>
2019-04-20 21:18:11 +0800

    剑指offer 二叉搜索树与双向链表

--- Convert.cpp
+++ Convert.cpp
@@ -14,26 +14,32 @@ class Solution {
 public:
     TreeNode* Convert(TreeNode* pRootOfTree)
     {
+        // lastNode指向双向链表的尾结点
         TreeNode* lastNode =NULL;
         TreeNode* headNode=ConvertNode(pRootOfTree, lastNode);
+        // headNode指向根节点root，往左移动到链表的头结点
         while (headNode != NULL && headNode->left != NULL) {
             headNode = headNode->left;
         }
         return headNode;
 
     }
+    // 该算法没有返回链表头，而是返回了root
     TreeNode* ConvertNode(TreeNode* rootTree, TreeNode* lastNode) {
         if (rootTree == NULL) {
             return NULL;
         }
+        // 转换左子树
         if (rootTree->left != NULL) {
             lastNode=ConvertNode(rootTree->left, lastNode);
         }
+        // 与根节点的衔接
         rootTree->left = lastNode;
         if (lastNode != NULL) {
             lastNode->right = rootTree;
         }
         lastNode = rootTree;
+        // 转换右子树
         if (rootTree->right != NULL) {
             lastNode=ConvertNode(rootTree->right, lastNode);
         }
